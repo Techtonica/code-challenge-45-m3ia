9174e17c50ff4b215773a044d2231d75188eaa67
Meia Natividad <meianatividad@gmail.com>
2021-02-09 16:28:33 -0800

    update function to compare 1st and 2nd elements

--- challenges/45.js
+++ challenges/45.js
@@ -10,18 +10,12 @@
 
 
 function max_end3 (int_array) {
-  for (i=0; i<int_array.length; i++) {
-
-    if (int_array[i+1] > int_array[i]) {
-      int_array[i] = int_array[i+1];
-    } else if (int_array[i] > int_array[i+1]) {
-      int_array[i+1] = int_array[i];
+  for (let i=0; i<int_array.length; i++) {
+    if (int_array[0] > int_array[int_array.length-1]) {
+    int_array[i] = int_array[0];
+  } else {
+    int_array[i] = int_array[int_array.length-1];
     }
-
-    if (int_array[i] > int_array[0]) {
-      int_array[0] = int_array[i];
-    }
-
   }
   return int_array;
 }
@@ -29,8 +23,8 @@ function max_end3 (int_array) {
 // trial lines:
 // console.log(max_end3([1, 2, 3])); // [3, 3, 3] 
 // console.log(max_end3([11, 5, 9])); // [11, 11, 11]
-// console.log(max_end3([1, 2, 3])); // [3, 3, 3]
+// console.log(max_end3([2, 11, 3])); // [3, 3, 3]
 
 // test line:
-module.exports = max_end3;
+// module.exports = max_end3;
 
